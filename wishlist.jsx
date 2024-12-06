e73a6955e324ba59849ef599fc63f7b50f09955c
kholoud Ayman <kholoudayman84@gmail.com>
2024-12-06 19:52:56 +0200

    Add files via upload

---
+++ wishlist.jsx
@@ -0,0 +1,117 @@
+<div className="Frame1 w-[1425px] h-[992px] relative bg-white">
+  <img className="Photo20241116025713 w-[268px] h-[243px] left-[88px] top-[139px] absolute" src="https://via.placeholder.com/268x243" />
+  <div className="Rectangle2 w-[268px] h-10 left-[88px] top-[342px] absolute bg-[#1e1e1e]" />
+  <div className="AddToCart left-[179px] top-[353px] absolute text-white text-base font-normal font-['Inter']">Add to cart</div>
+  <div className="Ellipse1 w-14 h-[37px] left-[283px] top-[152px] absolute bg-white/80 rounded-full" />
+  <div className="Trash w-[51px] h-[39px] left-[288px] top-[152px] absolute">
+    <img className="Subtract w-[38.25px] h-[24.38px] left-[6.38px] top-[9.75px] absolute" src="https://via.placeholder.com/38x24" />
+    <div className="Ellipse45 w-[8.50px] h-[1.62px] left-[29.75px] top-[4.88px] absolute origin-top-left rotate-180 rounded-full border-2 border-[#222222]" />
+  </div>
+  <div className="BasketAlt3Fill w-[54px] h-[31px] left-[118px] top-[346px] absolute bg-[#222222]">
+    <div className="Ellipse167 w-[4.50px] h-[2.58px] left-[36px] top-[24.54px] absolute bg-white rounded-full" />
+    <div className="Ellipse168 w-[4.50px] h-[2.58px] left-[18px] top-[24.54px] absolute bg-white rounded-full" />
+  </div>
+  <img className="Photo20241116025837 w-[268px] h-[210px] left-[409px] top-[139px] absolute" src="https://via.placeholder.com/268x210" />
+  <div className="Rectangle4 w-[268px] h-[39px] left-[409px] top-[343px] absolute bg-[#1e1e1e]" />
+  <div className="AddToCart w-[86px] h-[18px] left-[500px] top-[354px] absolute text-white text-base font-normal font-['Inter']">Add to cart</div>
+  <div className="Ellipse2 w-14 h-9 left-[604px] top-[159px] absolute bg-white/80 rounded-full" />
+  <div className="Trash w-[51px] h-[38px] left-[609px] top-[159px] absolute">
+    <img className="Subtract w-[38.25px] h-[23.75px] left-[6.38px] top-[9.50px] absolute" src="https://via.placeholder.com/38x24" />
+    <div className="Ellipse45 w-[8.50px] h-[1.58px] left-[29.75px] top-[4.75px] absolute origin-top-left rotate-180 rounded-full border-2 border-[#222222]" />
+  </div>
+  <div className="BasketAlt3Fill w-[54px] h-[30px] left-[439px] top-[347px] absolute bg-[#222222]">
+    <div className="Ellipse167 w-[4.50px] h-[2.50px] left-[36px] top-[23.75px] absolute bg-white rounded-full" />
+    <div className="Ellipse168 w-[4.50px] h-[2.50px] left-[18px] top-[23.75px] absolute bg-white rounded-full" />
+  </div>
+  <img className="Photo20241116025843 w-[268px] h-[243px] left-[747px] top-[139px] absolute" src="https://via.placeholder.com/268x243" />
+  <div className="Rectangle6 w-[268px] h-10 left-[747px] top-[342px] absolute bg-[#1e1e1e]" />
+  <div className="AddToCart left-[838px] top-[353px] absolute text-white text-base font-normal font-['Inter']">Add to cart</div>
+  <div className="Ellipse3 w-14 h-[37px] left-[942px] top-[152px] absolute bg-white/80 rounded-full" />
+  <div className="Trash w-[51px] h-[39px] left-[947px] top-[152px] absolute">
+    <img className="Subtract w-[38.25px] h-[24.38px] left-[6.38px] top-[9.75px] absolute" src="https://via.placeholder.com/38x24" />
+    <div className="Ellipse45 w-[8.50px] h-[1.62px] left-[29.75px] top-[4.88px] absolute origin-top-left rotate-180 rounded-full border-2 border-[#222222]" />
+  </div>
+  <div className="BasketAlt3Fill w-[54px] h-[31px] left-[777px] top-[346px] absolute bg-[#222222]">
+    <div className="Ellipse167 w-[4.50px] h-[2.58px] left-[36px] top-[24.54px] absolute bg-white rounded-full" />
+    <div className="Ellipse168 w-[4.50px] h-[2.58px] left-[18px] top-[24.54px] absolute bg-white rounded-full" />
+  </div>
+  <img className="Photo20241116025845 w-[268px] h-[243px] left-[1085px] top-[139px] absolute" src="https://via.placeholder.com/268x243" />
+  <div className="Rectangle8 w-[268px] h-10 left-[1085px] top-[342px] absolute bg-[#1e1e1e]" />
+  <div className="AddToCart left-[1176px] top-[353px] absolute text-white text-base font-normal font-['Inter']">Add to cart</div>
+  <div className="Ellipse4 w-14 h-[37px] left-[1280px] top-[152px] absolute bg-white/80 rounded-full" />
+  <div className="Trash w-[51px] h-[39px] left-[1285px] top-[152px] absolute">
+    <img className="Subtract w-[38.25px] h-[24.38px] left-[6.38px] top-[9.75px] absolute" src="https://via.placeholder.com/38x24" />
+    <div className="Ellipse45 w-[8.50px] h-[1.62px] left-[29.75px] top-[4.88px] absolute origin-top-left rotate-180 rounded-full border-2 border-[#222222]" />
+  </div>
+  <div className="BasketAlt3Fill w-[54px] h-[31px] left-[1115px] top-[346px] absolute bg-[#222222]">
+    <div className="Ellipse167 w-[4.50px] h-[2.58px] left-[36px] top-[24.54px] absolute bg-white rounded-full" />
+    <div className="Ellipse168 w-[4.50px] h-[2.58px] left-[18px] top-[24.54px] absolute bg-white rounded-full" />
+  </div>
+  <img className="Photo20241116025826 w-[268px] h-[243px] left-[88px] top-[610.15px] absolute" src="https://via.placeholder.com/268x243" />
+  <div className="Rectangle10 w-[268px] h-10 left-[88px] top-[813.15px] absolute bg-[#1e1e1e]" />
+  <div className="AddToCart left-[179px] top-[824.15px] absolute text-white text-base font-normal font-['Inter']">Add to cart</div>
+  <div className="Ellipse5 w-14 h-[37px] left-[283px] top-[623.15px] absolute bg-white/80 rounded-full" />
+  <div className="BasketAlt3Fill w-[54px] h-[31px] left-[118px] top-[817.15px] absolute bg-[#222222]">
+    <div className="Ellipse167 w-[4.50px] h-[2.58px] left-[36px] top-[24.54px] absolute bg-white rounded-full" />
+    <div className="Ellipse168 w-[4.50px] h-[2.58px] left-[18px] top-[24.54px] absolute bg-white rounded-full" />
+  </div>
+  <img className="Photo20241116025831 w-[268px] h-[243px] left-[406px] top-[610.15px] absolute" src="https://via.placeholder.com/268x243" />
+  <div className="Rectangle14 w-[268px] h-10 left-[406px] top-[813.15px] absolute bg-[#1e1e1e]" />
+  <div className="AddToCart left-[497px] top-[824.15px] absolute text-white text-base font-normal font-['Inter']">Add to cart</div>
+  <div className="Ellipse7 w-14 h-[37px] left-[601px] top-[623.15px] absolute bg-white/80 rounded-full" />
+  <div className="BasketAlt3Fill w-[54px] h-[31px] left-[436px] top-[817.15px] absolute bg-[#222222]">
+    <div className="Ellipse167 w-[4.50px] h-[2.58px] left-[36px] top-[24.54px] absolute bg-white rounded-full" />
+    <div className="Ellipse168 w-[4.50px] h-[2.58px] left-[18px] top-[24.54px] absolute bg-white rounded-full" />
+  </div>
+  <img className="Photo20241116025833 w-[268px] h-[248px] left-[1085px] top-[610px] absolute" src="https://via.placeholder.com/268x248" />
+  <div className="Rectangle18 w-[268px] h-[43px] left-[1085px] top-[813.15px] absolute bg-[#1e1e1e]" />
+  <div className="AddToCart w-[86px] h-5 left-[1176px] top-[825.15px] absolute text-white text-base font-normal font-['Inter']">Add to cart</div>
+  <div className="Ellipse9 w-14 h-[39px] left-[1280px] top-[611.15px] absolute bg-white/80 rounded-full" />
+  <div className="BasketAlt3Fill w-[54px] h-[33px] left-[1115px] top-[818.15px] absolute bg-[#222222]">
+    <div className="Ellipse167 w-[4.50px] h-[2.75px] left-[36px] top-[26.12px] absolute bg-white rounded-full" />
+    <div className="Ellipse168 w-[4.50px] h-[2.75px] left-[18px] top-[26.12px] absolute bg-white rounded-full" />
+  </div>
+  <img className="Photo20241116025221 w-[268px] h-[243px] left-[753px] top-[608.15px] absolute" src="https://via.placeholder.com/268x243" />
+  <div className="Rectangle20 w-[268px] h-10 left-[753px] top-[811.15px] absolute bg-[#1e1e1e]" />
+  <div className="AddToCart left-[844px] top-[822.15px] absolute text-white text-base font-normal font-['Inter']">Add to cart</div>
+  <div className="Ellipse10 w-14 h-[37px] left-[948px] top-[621.15px] absolute bg-white/80 rounded-full" />
+  <div className="BasketAlt3Fill w-[54px] h-[31px] left-[783px] top-[815.15px] absolute bg-[#222222]">
+    <div className="Ellipse167 w-[4.50px] h-[2.58px] left-[36px] top-[24.54px] absolute bg-white rounded-full" />
+    <div className="Ellipse168 w-[4.50px] h-[2.58px] left-[18px] top-[24.54px] absolute bg-white rounded-full" />
+  </div>
+  <div className="Rectangle42 w-[25px] h-[48.71px] left-[92.40px] top-[556.71px] absolute origin-top-left rotate-[0.70deg] opacity-40 bg-[#eb221e] rounded" />
+  <div className="JustFourYou left-[131px] top-[517.15px] absolute text-black text-2xl font-normal font-['Inter']">just four you</div>
+  <div className="Rectangle43 w-[174px] h-11 left-[1175px] top-[513.15px] absolute bg-[#d9d9d9]" />
+  <div className="SeeAll left-[1225px] top-[519.15px] absolute text-black text-2xl font-normal font-['Inter']">see All</div>
+  <div className="Rectangle44 w-[215px] h-[39px] left-[1169px] top-[71px] absolute bg-[#d9d9d9]" />
+  <div className="MoveAllToTag left-[1191px] top-[76px] absolute text-black text-2xl font-normal font-['Inter']">move All to tag</div>
+  <div className="100 left-[869px] top-[433px] absolute text-[#eb221e] text-xl font-normal font-['Inter']">$100</div>
+  <div className="Wishlist4 left-[102px] top-[83px] absolute text-black text-2xl font-normal font-['Inter']">wishlist(4)</div>
+  <div className="View w-[33px] h-[29px] left-[295px] top-[627px] absolute">
+    <div className="Ellipse65 w-[11px] h-[9.67px] left-[11px] top-[9.67px] absolute rounded-full border-2 border-[#33363f]" />
+  </div>
+  <div className="BagCeline w-[116px] h-[23px] left-[118px] top-[395px] absolute text-black text-xl font-normal font-['Inter']">bag celine</div>
+  <div className="PlayStation5 left-[423px] top-[399px] absolute text-black text-xl font-normal font-['Inter']">play station5</div>
+  <div className="TshirtGucci left-[797px] top-[399px] absolute text-black text-xl font-normal font-['Inter']">tshirt gucci</div>
+  <div className="BagFendi left-[1167px] top-[395px] absolute text-black text-xl font-normal font-['Inter']">bag fendi</div>
+  <div className="LaptopLenovo left-[112px] top-[861.15px] absolute text-black text-xl font-normal font-['Inter']">laptop lenovo</div>
+  <div className="BagHermes left-[810px] top-[866.15px] absolute text-black text-xl font-normal font-['Inter']">bag hermes</div>
+  <div className="LaptopLenovo left-[423px] top-[866.15px] absolute text-black text-xl font-normal font-['Inter']">laptop lenovo</div>
+  <div className="LaptopLenovoLegion5 left-[1115px] top-[873.15px] absolute text-black text-xl font-normal font-['Inter']">laptop lenovo legion 5</div>
+  <div className="1200 left-[445px] top-[897.15px] absolute text-[#eb221e] text-xl font-normal font-['Inter']">$1200</div>
+  <div className="500 left-[147px] top-[892.15px] absolute text-[#eb221e] text-xl font-normal font-['Inter']">$500</div>
+  <div className="100 left-[850px] top-[897.15px] absolute text-[#eb221e] text-xl font-normal font-['Inter']">$100</div>
+  <div className="1900 left-[1189px] top-[897.15px] absolute text-[#eb221e] text-xl font-normal font-['Inter']">$1900</div>
+  <div className="1000 left-[445px] top-[431px] absolute text-[#eb221e] text-xl font-normal font-['Inter']">$1000</div>
+  <div className="300 left-[1189px] top-[426px] absolute text-[#eb221e] text-xl font-normal font-['Inter']">$300</div>
+  <div className="250 left-[145px] top-[431px] absolute text-[#eb221e] text-xl font-normal font-['Inter']">$250</div>
+  <div className="View w-[33px] h-[29px] left-[609px] top-[627px] absolute">
+    <div className="Ellipse65 w-[11px] h-[9.67px] left-[11px] top-[9.67px] absolute rounded-full border-2 border-[#33363f]" />
+  </div>
+  <div className="View w-[33px] h-[29px] left-[960px] top-[627px] absolute">
+    <div className="Ellipse65 w-[11px] h-[9.67px] left-[11px] top-[9.67px] absolute rounded-full border-2 border-[#33363f]" />
+  </div>
+  <div className="View w-[33px] h-[29px] left-[1290px] top-[619px] absolute">
+    <div className="Ellipse65 w-[11px] h-[9.67px] left-[11px] top-[9.67px] absolute rounded-full border-2 border-[#33363f]" />
+  </div>
+</div>
